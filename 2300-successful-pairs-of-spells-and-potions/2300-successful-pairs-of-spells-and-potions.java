444d57485264ec1dfc60e6b7a1d7488bd5236b04
최장우 <97choijw@gmail.com>
2025-03-02 14:22:44 +0900

    Time: 39 ms (92.45%), Space: 67.3 MB (6.01%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -22,3 +22,25 @@ class Solution {
         return result;
     }
 }
+/*
+Discussion에 있는 이분탐색 템플릿
+def binarySearchForMinVal()
+    l, r = lower_bound, upper_bound
+    while l < r:
+        mid = (l + r)//2 #round down
+        if feasible(mid):
+            r = mid #check for possible smaller values that work
+        else:
+            l = mid + 1 #values smaller than or equal to mid do not work. Reduce search space to values greater than mid.
+    return l
+
+def binarySearchForMaxVal()
+    l, r = lower_bound, upper_bound
+    while l < r:
+        mid = (l + r + 1)//2 #round up
+        if feasible(mid):
+            l = mid #check for possible greater values that work
+        else:
+            r = mid - 1 #values greater than or equal to mid do not work. Reduce search space to values smaller than mid.
+    return l    
+*/
