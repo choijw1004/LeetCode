15e7c7b7013895c8384f12e88b0fe453255dd93a
최장우 <97choijw@gmail.com>
2025-03-15 16:37:43 +0900

    Time: 0 ms (100%), Space: 40.6 MB (27.29%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -3,7 +3,7 @@ class Solution {
         int[] fibo = new int[n+1];
         if(n == 0) return 0;
         if(n==1) return 1;
-        if(n==2) return 2;
+        if(n==2) return 1;
         fibo[0] = 0;
         fibo[1] = 1;
         fibo[2] = 1;
