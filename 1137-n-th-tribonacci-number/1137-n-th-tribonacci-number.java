e6413ca0722b5d6ae65187b29812e9d2f9221ab4
최장우 <97choijw@gmail.com>
2025-03-15 16:37:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] fibo = new int[n+1];
+        if(n == 0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        fibo[0] = 0;
+        fibo[1] = 1;
+        fibo[2] = 1;
+
+        for(int i = 3; i<= n; i++){
+            fibo[i] = fibo[i-1] + fibo[i-2] + fibo[i-3];
+        }
+        return fibo[n];
+    }
+}
