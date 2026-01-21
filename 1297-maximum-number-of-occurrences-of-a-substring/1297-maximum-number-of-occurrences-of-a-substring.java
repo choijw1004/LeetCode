f0b13815d2bce0fdc23947e54c7d0955c3956aec
최장우 <97choijw@gmail.com>
2026-01-21 23:08:09 +0900

    Time: 75 ms (20.51%), Space: 49.2 MB (22.93%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
@@ -1,26 +1,47 @@
 class Solution {
-    public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        int n = s.length();
-        Map<String, Integer> freq = new HashMap<>();
+    int ml;
 
-        for (int i = 0; i + minSize <= n; i++) {
-            String sub = s.substring(i, i + minSize);
-            if (countUnique(sub) <= maxLetters) {
-                freq.put(sub, freq.getOrDefault(sub, 0) + 1);
+    private boolean isValid(String s){
+        HashSet<Character> set = new HashSet<>();
+        
+        for(int i = 0 ; i < s.length(); i++){
+            set.add(s.charAt(i));
+        }
+
+        return set.size() <= ml ? true : false;
+    }
+    public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+        int i = 0, j = 0;  
+        int size = minSize;
+        int n = s.length();
+        this.ml = maxLetters;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+
+        while(j < n){
+            if(j - i + 1 < size){
+                sb.append(s.charAt(j));
+                j++;
+            }
+
+            else{
+                sb.append(s.charAt(j));
+
+                if(isValid(sb.toString())){
+                    map.put(sb.toString(), map.getOrDefault(sb.toString(),0) + 1);
+                }
+                sb.deleteCharAt(0);
+                i++;
+                j++;
             }
         }
 
-        int ans = 0;
-        for (int v : freq.values()) 
-            ans = Math.max(ans, v);
-        return ans;
-    }
+        List<Map.Entry<String,Integer>> list = new ArrayList<>(map.entrySet());
 
-    private int countUnique(String t) {
-        Set<Character> set = new HashSet<>();
-        for (char c : t.toCharArray()) {
-            set.add(c);
-        }
-        return set.size();
+        Collections.sort(list, (a,b) -> b.getValue() - a.getValue());
+
+
+        return list.size() == 0 ? 0 : list.get(0).getValue();
     }
 }
