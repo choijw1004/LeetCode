8b2548fec163b283f744af22c76a554076b3142d
최장우 <97choijw@gmail.com>
2026-01-21 22:08:18 +0900

    Time: 14 ms (58.15%), Space: 46.6 MB (44.79%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,23 +1,49 @@
 class Solution {
     private boolean isVowel(Character c){
-        return (c == 'a' || c == 'e'|| c == 'i' || c == 'o' || c == 'u');
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
+
+    private int makeHash(Character c){
+        if(c == 'a') return 0;
+        else if(c == 'e') return 1;
+        else if(c == 'i') return 2;
+        else if(c == 'o') return 3;
+        else return 4;
+
+    }
+
     public int maxVowels(String s, int k) {
-
+        int i = 0, j = 0, ans = 0;
+        int size = k;
         int n = s.length();
-        int cnt = 0;
-        
-        for(int i = 0 ; i < k; i++){
-            char c = s.charAt(i);
-            if(isVowel(c)) cnt++;
-        }
-        int ans = cnt;
+        int[] hash = new int[5];
 
-        for(int i = k; i < n; i++){
-            if(isVowel(s.charAt(i))) cnt++;
-            if(isVowel(s.charAt(i-k))) cnt--;
-            
-            ans = Math.max(ans,cnt);
+        while(j < n){
+            if(j - i + 1 < size){
+                if(isVowel(s.charAt(j))){
+                    hash[makeHash(s.charAt(j))]++;
+                }
+                j++;
+            }
+            else{
+                if(isVowel(s.charAt(j))){
+                    hash[makeHash(s.charAt(j))]++;
+                }
+                int tmp = 0;
+
+                for(int num : hash){
+                    tmp += num;
+                }
+                ans = Math.max(tmp, ans);
+                
+                if(isVowel(s.charAt(i))){
+                    hash[makeHash(s.charAt(i))]--;
+                }
+
+                i++;
+                j++;
+            }
+
         }
         return ans;
     }
