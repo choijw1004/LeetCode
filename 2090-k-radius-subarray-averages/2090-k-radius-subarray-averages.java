9a5195b0501f572891b6324cc79620287c9898c3
최장우 <97choijw@gmail.com>
2026-01-22 15:12:28 +0900

    Time: 6 ms (72.23%), Space: 175.2 MB (55.87%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,23 +1,35 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
         int n = nums.length;
-        int[] answer = new int[n];
-        Arrays.fill(answer, -1);
-        if (2*k + 1 > n) return answer; 
+        int[] ans = new int[n];
+        int i = 0;
+        int j = 0;
+        long size = 2 * k + 1;
+        long sum = 0;
+        Arrays.fill(ans,-1);
 
-        long[] prefix = new long[n];
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + nums[i];
+        if(k == 0) return nums;
+
+
+        while(j < n){
+            if(j - i + 1 < size){
+                sum += nums[j];
+                j++;
+            }
+
+            else{
+                sum += nums[j];
+                ans[(i + j) / 2] = (int)(sum / size);
+
+                sum -= nums[i];
+                
+                i++;
+                j++;
+            }
+
         }
 
-        int window = 2*k + 1;
-        for (int i = k; i + k < n; i++) {
-            int left = i - k, right = i + k;
-            long sum = prefix[right] - (left > 0 ? prefix[left-1] : 0L);
-            answer[i] = (int)(sum / window);
-        }
-
-        return answer;
+        return ans;
+        
     }
 }
