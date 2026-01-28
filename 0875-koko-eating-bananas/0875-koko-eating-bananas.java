da307da439b080336ff79e07d41f7ee91b2a817e
최장우 <97choijw@gmail.com>
2026-01-28 13:08:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,31 +1,40 @@
 class Solution {
-    private int calcuTime(int[] piles, int k) {
-        int time = 0;
-        for (int p : piles) {
-            time += (p + k - 1) / k;
-        }
-        return time;
-    }
-    
-    public int minEatingSpeed(int[] piles, int h) {
-        int maxPile = 0;
+    int[] piles;
+    int h;
 
-        for (int p : piles) {
-            maxPile = Math.max(maxPile, p);
+    public int minEatingSpeed(int[] piles, int h) {
+        this.piles = piles;
+        this.h = h;
+        long ans = 0;
+        long max = -1;
+
+        for(int p : piles){
+            max = Math.max(p,max);
         }
         
-        int left = 1;
-        int right = maxPile;  
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (calcuTime(piles, mid) <= h) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        long start = 1;
+        long end = max;
+
+        while(start <= end){
+            long mid = (start + end) / 2;
+
+            if(canEat(mid)) {
+                ans = mid;
+                end = mid -1;
             }
+            else start = mid + 1;
         }
-        return left;
+
+        return (int)ans;
+    }
+
+    private boolean canEat(long time){
+        int cnt = 0;
+
+        for(int p : piles){
+            cnt += (p + time -1)/ time;
+        }
+
+        return cnt <= h ? true : false;
     }
 }
